aadef57d572fe9242d1941a4580ccec876a49a61
austin1997 <zhaiz@rose-hulman.edu>
2017-11-04 22:10:07 -0400

    train

--- cifar10_dist2.py
+++ cifar10_dist2.py
@@ -199,6 +199,8 @@ def map_fun(args, ctx):
           
       # Calculate the average cross entropy loss across the batch.
       labels = y_
+      print (labels.shape)
+      print (logits.shape)
       labels = tf.cast(labels, tf.int64)
       cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
           labels=labels, logits=logits, name='cross_entropy_per_example')
